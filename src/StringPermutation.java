9ca98c5e400af7f550c8294554987229a1c2254b
Vrajesh Akabari <vrajesh99999@gmail.com>
2019-05-13 17:34:21 -0700

    Initial commit

--- src/StringPermutation.java
+++ src/StringPermutation.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class StringPermutation {
+    public static ArrayList<String> permutation(String s){
+        ArrayList<String> result = new ArrayList<>();
+        result = permute("",s,result);
+        return result;
+    }
+    public static ArrayList<String> permute(String prefix,String suffix, ArrayList<String> result){
+        if(suffix.length()==0){
+            result.add(prefix);
+        }else{
+            for(int i=0;i<suffix.length();i++){
+                permute(prefix+suffix.charAt(i),suffix.substring(0,i)+suffix.substring(i+1,suffix.length()),result);
+            }
+        }
+        return result;
+    }
+    public static void main(String args[]){
+        ArrayList<String> result = permutation("ABC");
+        System.out.println(result);
+    }
+
+
 }
